00690e135312ea8a14cb29b2a2c9bacfecf50583
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2025-01-19 03:46:46 +0530

    preview gallery images

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,7 +1,18 @@
 import styles from "../styles/Gallery.module.css";
 import { images } from "../data";
+import { useState } from "react";
 
 export default function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  function preview(url) {
+    setSelectedImage(url);
+  }
+
+  function close() {
+    setSelectedImage(null);
+  }
+
   return (
     <>
       <div className={styles.containerDiv}>
@@ -9,10 +20,26 @@ export default function Gallery() {
 
         <div className={styles.galleryDiv}>
           {images.map((url, i) => (
-            <img key={i} src={url} alt="gallery-img" />
+            <img
+              key={i}
+              src={url}
+              alt="gallery-img"
+              onClick={() => preview(url)}
+            />
           ))}
         </div>
       </div>
+
+      {selectedImage && (
+        <div className={styles.modal} onClick={close}>
+          <div className={styles.modalContent}>
+            <img src={selectedImage} alt="preview-image" />
+            <span class="material-symbols-outlined" onClick={close}>
+              close
+            </span>
+          </div>
+        </div>
+      )}
     </>
   );
 }
